6ccbc064fd5574c57755ad89f2bc9d3365513d71
nurhena <91648830+nurhena@users.noreply.github.com>
2023-10-13 23:12:20 +0700

    Add files via upload

---
+++ src/test/groovy/com/jep/tgs2/Tgs2ApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.jep.tgs2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Tgs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
